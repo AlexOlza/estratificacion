55b5b865b6fc80d12b9c6d0c56d9c653ee8eca88
AlexOlza <aolza@bcamath.org>
2021-12-15 16:35:40 +0100

    Attempt to fix issue #8

--- configurations/cluster/default.py
+++ configurations/cluster/default.py
@@ -5,7 +5,7 @@ Created on Thu Nov 18 16:14:16 2021
 
 @author: aolza
 """
-assert False, "looks like obsolete module"
+# assert False, "looks like obsolete module"
 from configurations.default import *
 CONFIGPATH=ROOTPATH+'configurations/cluster/'
 USEDCONFIGPATH=ROOTPATH+'configurations/used/'
