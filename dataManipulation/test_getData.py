76c611220da389263b711da22073b7e942d3be03
AlexOlza <aolza@bcamath.org>
2022-01-12 11:46:54 +0100

    Test that we are able to recover the old response variable

--- dataManipulation/test_getData.py
+++ dataManipulation/test_getData.py
@@ -7,5 +7,19 @@ Created on Wed Jan 12 11:19:22 2022
 @author: aolza
 """
 from dataManipulation.dataPreparation import getData
+import pandas as pd
 _,y=getData(2016,oldbase=False)
+outcome=list(y.columns)
+outcome.remove('PATIENT_ID')
+
 _,yOLD=getData(2016,oldbase=True)
+
+df=pd.DataFrame()
+df['id']=y.PATIENT_ID
+df['OLD']=yOLD.ingresoUrg
+df['NEW']=y[outcome]
+
+print('Patients with different outcome with the new hospitalizatiton data:')
+print(df[df.OLD!=df.NEW])
+
+print('NOTE: SOME SMALL DIFFERENCES ARE NORMAL')
