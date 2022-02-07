46914344f233362ef3a354eef2bc47b15d6e1d8e
AlexOlza <aolza@bcamath.org>
2022-02-07 13:21:59 +0100

    Add nested models

--- main/cluster/nested_logistic.py
+++ main/cluster/nested_logistic.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-assert False, "Mischievous filename! Review naming policy"
 """
-Created on Thu Nov 25 16:23:21 2021
+Assess the predictive performance gain
+of the ACG variable, task #48 
+Created on Mon Feb  7 12:30:42 2022
 
 @author: aolza
+What should I do with these? Ask Edu
+HOSDOM|FRAILTY|INGRED_14GT
 """
 import sys
 sys.path.append('/home/aolza/Desktop/estratificacion/')#necessary in cluster
 
-chosen_config='configurations.local.'+sys.argv[1]
+chosen_config='configurations.cluster.'+sys.argv[1]
 experiment='configurations.'+sys.argv[2]
 import importlib
 importlib.invalidate_caches()
@@ -23,21 +26,14 @@ util.makeAllPaths()
 
 from dataManipulation.dataPreparation import getData
 import numpy as np
+from time import time
 from sklearn.linear_model import LogisticRegression
 
 #%%
 np.random.seed(config.SEED)
 
-X,y=getData(2016,oldbase=False)#new data 
+X,y=getData(2016)#new data 
 #%%
-
-y=np.where(y[config.COLUMNS]>=1,1,0)
-y=y.ravel()
-print('Sample size ',len(X), 'positive: ',sum(y))
-# assert False
-#%%
-logistic=LogisticRegression(penalty='none',max_iter=1000,verbose=0)
-
 to_drop=['PATIENT_ID','ingresoUrg']
 for c in to_drop:
     try:
@@ -46,10 +42,22 @@ for c in to_drop:
     except:
         pass
         util.vprint('pass')
-from time import time
-t0=time()
-fit=logistic.fit(X, y)
-print('fitting time: ',time()-t0)
-#%%
-util.savemodel(config, fit)
+
+y=np.where(y[config.COLUMNS]>=1,1,0)
+y=y.ravel()
+print('Sample size ',len(X), 'positive: ',sum(y))
+# assert False
+variable_groups=[r'FEMALE|AGE_[0-9]+$','EDC_','RXMG_','ACG']
+
+for i in range(1,len(variable_groups)+1):
+    regex=r'|'.join(variable_groups[:i])
+    Xx=X.filter(regex=regex)
+    print('Number of predictors: ',len(Xx.columns))
+    logistic=LogisticRegression(penalty='none',max_iter=1000,verbose=0) 
+    t0=time()
+    fit=logistic.fit(Xx, y)
+    print('fitting time: ',time()-t0)
+    #%%
+    util.savemodel(config, fit)
+
 
