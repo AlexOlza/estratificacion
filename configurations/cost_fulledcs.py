d41d23d491ea274d2634b3e3b2200beceb01c5a5
AlexOlza <aolza@bcamath.org>
2022-02-22 16:27:15 +0100

    Add full edcs experiments

--- configurations/cost_fulledcs.py
+++ configurations/cost_fulledcs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  9 11:32:19 2021
+Created on Mon Jan 31 2022
 
 @author: aolza
 """
 
-EXPERIMENT='urgcms_excl_hdia_nbinj'
+EXPERIMENT='cost_fulledcs'
+FULLEDCS=True
 
 """PREDICTORS: They will be different for each script."""
 PREDICTORREGEX=r'PATIENT_ID|FEMALE|AGE_[0-9]+$|ACG|EDC_|HOSDOM|FRAILTY|RXMG_|INGRED_14GT'
 INDICEPRIVACION=False
-COLUMNS=['urgcms']#variable respuesta
+COLUMNS=['COSTE_TOTAL_ANO2']#variable respuesta
 PREVIOUSHOSP=[]
-EXCLUDE=['hdia','nbinj']
+EXCLUDE=[]
 #Exclude patients from Tolosaldea and Errioxa because they receive
 #most of their care outside of Osakidetza.
 EXCLUDEOSI=['OS16','OS22'] 
--- configurations/urgcms_excl_nbinj_fulledcs.py
+++ configurations/urgcms_excl_nbinj_fulledcs.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  9 11:32:19 2021
+Created on Mon Jan 31 2022
 
 @author: aolza
 """
 
-EXPERIMENT='urgcms_excl_hdia_nbinj'
+EXPERIMENT='urgcms_excl_nbinj_fulledcs'
+FULLEDCS=True
 
 """PREDICTORS: They will be different for each script."""
 PREDICTORREGEX=r'PATIENT_ID|FEMALE|AGE_[0-9]+$|ACG|EDC_|HOSDOM|FRAILTY|RXMG_|INGRED_14GT'
 INDICEPRIVACION=False
 COLUMNS=['urgcms']#variable respuesta
 PREVIOUSHOSP=[]
-EXCLUDE=['hdia','nbinj']
+EXCLUDE=['nbinj']
 #Exclude patients from Tolosaldea and Errioxa because they receive
 #most of their care outside of Osakidetza.
 EXCLUDEOSI=['OS16','OS22'] 
