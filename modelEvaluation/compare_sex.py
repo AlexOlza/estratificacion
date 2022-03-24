eceeb6246545a6c34e8e2875c485b5c7c9d79214
AlexOlza <aolza@bcamath.org>
2022-03-24 18:04:31 +0100

    Calibrate sex comparison

--- modelEvaluation/compare_sex.py
+++ modelEvaluation/compare_sex.py
@@ -12,7 +12,6 @@ from sklearn.metrics import roc_auc_score, RocCurveDisplay,roc_curve, auc,precis
 from modelEvaluation.predict import generate_filename
 import sys
 sys.path.append('/home/aolza/Desktop/estratificacion/')
-
 import pandas as pd
 #%%
 
@@ -22,12 +21,12 @@ from python_settings import settings as config
 import configurations.utility as util
 if not config.configured: 
     configuration=util.configure(config_used)
-
+# sys.stdout = open(f'{config.ROOTPATH}estratificacion-reports/compare_sex_{config.EXPERIMENT}.md', 'w')
 from modelEvaluation.compare import detect_models, compare, detect_latest, performance
+import modelEvaluation.calibrate as cal
 from dataManipulation.dataPreparation import getData
 #%%
-def plot_roc(y, pred, groupname):
-    fpr, tpr, thresholds = roc_curve(y, pred)
+def plot_roc(fpr, tpr, groupname):
     roc_auc = auc(fpr, tpr)
     display = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc,
                       estimator_name=groupname)
@@ -35,8 +34,7 @@ def plot_roc(y, pred, groupname):
     
     return display
 
-def plot_pr(y, pred, groupname):
-    precision, recall, _ = precision_recall_curve(y, pred)
+def plot_pr(precision, recall, groupname):
     display = PrecisionRecallDisplay(precision=precision, recall=recall, estimator_name=groupname)
     return display
 def beta_differences(modelname1, modelname2, features):
@@ -61,6 +59,17 @@ def top_K_dict(d, K, reverse=True):
 #%%
 year=int(input('YEAR TO PREDICT: ')) 
 X,y=getData(year-1)
+pastX,pasty=getData(year-2)
+#%%
+# X.drop('PATIENT_ID', axis=1, inplace=True)
+predictors=X.columns
+features=X.drop('PATIENT_ID', axis=1).columns
+
+for column in features:
+    if column!='FEMALE':
+        X[f'{column}INTsex']=X[column]*X['FEMALE']
+        pastX[f'{column}INTsex']=pastX[column]*pastX['FEMALE']
+#%%
 available_models=detect_models()
 # latest=detect_latest(available_models)
 
@@ -69,62 +78,112 @@ male=X['FEMALE']==0
 sex=['Mujeres', 'Hombres']
 #%%
 import joblib as job
-roc,roc_joint={},{}
-pr, pr_joint={}, {} #precision-recall curves
+roc,roc_joint,roc_inter={k:{} for k in sex},{k:{} for k in sex},{k:{} for k in sex}
+pr, pr_joint, pr_inter={k:{} for k in sex}, {k:{} for k in sex}, {k:{} for k in sex} #precision-recall curves
+PRECISION, RECALL, THRESHOLDS={k:{} for k in sex}, {k:{} for k in sex}, {k:{} for k in sex}
+FPR,TPR, ROCTHRESHOLDS={k:{} for k in sex}, {k:{} for k in sex}, {k:{} for k in sex}
+
 table=pd.DataFrame()
 K=20000
-for group, groupname in zip([female,male],sex):
-    selected=[l for l in available_models if ((groupname in l ) or (bool(re.match('logistic\d+',l))))]
-    print(selected)
-    globalmodel=list(set(selected)-set([f'logistic{groupname}']))[0]+'.joblib'
-    separatemodel=f'logistic{groupname}.joblib'
-    globalmodel=job.load(config.MODELPATH+globalmodel)
-    separatemodel=job.load(config.MODELPATH+separatemodel)
+models=['Global', 'Separado', 'Interaccion']
+fighist, (axhist,axhist2) = plt.subplots(1,2)
+for i, group, groupname in zip([1,0],[female,male],sex):
+    recall,ppv,spec, score={},{},{},{}
+    selected=[l for l in available_models if ((groupname in l ) or (bool(re.match('logistic\d+|logisticSexInteraction',l))))]
+    print('Selected models: ',selected)
+    # LOAD MODELS
+    globalmodelname=list(set(selected)-set([f'logistic{groupname}'])-set(['logisticSexInteraction']))[0]
+    separatemodelname=f'logistic{groupname}.joblib'
+    globalmodel=job.load(config.MODELPATH+globalmodelname+'.joblib')
+    interactionmodel=job.load(config.MODELPATH+'logisticSexInteraction.joblib')
+    separatemodel=job.load(config.MODELPATH+separatemodelname)
+    
+    # SUBSET DATA
     Xgroup=X.loc[group]
     ygroup=y.loc[group]
+    pastXgroup=pastX.loc[pastX['FEMALE']==i]
+    pastygroup=pasty.loc[pastX['FEMALE']==i]
+
+    # Xgroup.drop('PATIENT_ID', axis=1, inplace=True)
+    # pastXgroup.drop('PATIENT_ID', axis=1, inplace=True)
     assert (all(Xgroup['FEMALE']==1) or all(Xgroup['FEMALE']==0))
-    # ygroup=np.where(ygroup[config.COLUMNS]>=1,1,0)
-    # ygroup=ygroup.ravel()
     print('Sample size ',len(Xgroup), 'positive: ',sum(np.where(ygroup[config.COLUMNS]>=1,1,0)))  
     
-    separate_preds=separatemodel.predict_proba(Xgroup.drop(['PATIENT_ID','FEMALE'],axis=1))[:,-1]
-    joint_preds=globalmodel.predict_proba(Xgroup.drop(['PATIENT_ID'], axis=1))[:,-1]
-
+    # PREDICT
+    # separate_preds=separatemodel.predict_proba(Xgroup[features].drop(['FEMALE'],axis=1))[:,-1]
+    # joint_preds=globalmodel.predict_proba(Xgroup[features])[:,-1]
+    # inter_preds=interactionmodel.predict_proba(Xgroup)[:,-1]
+    # obs=np.where(ygroup[config.COLUMNS]>=1,1,0)
+    separate_cal=cal.calibrate(f'logistic{groupname}',year,
+                              presentX=Xgroup[predictors].drop('FEMALE', axis=1),
+                              presentY=ygroup,
+                              pastX=pastXgroup[predictors].drop('FEMALE', axis=1),
+                              pastY=pastygroup)
+    joint_cal=cal.calibrate(globalmodelname,year,
+                              presentX=Xgroup[predictors], presentY=ygroup,
+                              pastX=pastXgroup[predictors], pastY=pastygroup)
+    inter_cal=cal.calibrate('logisticSexInteraction',year,
+                              presentX=Xgroup, presentY=ygroup,
+                              pastX=pastXgroup[pastXgroup.columns],
+                              pastY=pastygroup,
+                              predictors=config.PREDICTORREGEX+'|INTsex')
+    inter_preds=inter_cal.PREDCAL
+    joint_preds=joint_cal.PREDCAL
+    separate_preds=separate_cal.PREDCAL
     obs=np.where(ygroup[config.COLUMNS]>=1,1,0)
+    axhist.hist(separate_preds,bins=1000,label=groupname)
+    axhist2.hist(joint_preds,bins=1000,label=groupname)
     
-    roc[groupname]=plot_roc(obs,separate_preds, groupname)
-    roc_joint[groupname]=plot_roc(obs,joint_preds, groupname)
-    pr_joint[groupname]=plot_pr(obs, joint_preds, groupname)
-    pr[groupname]=plot_pr(obs, separate_preds, groupname)
-    
-    recall,ppv=performance(separate_preds, obs, K)
-    score=roc_auc_score(obs, separate_preds)
-    recalljoint,ppvjoint=performance(joint_preds, obs, K)
-    scorejoint=roc_auc_score(obs, joint_preds)
-    df=pd.DataFrame(list(zip([f'Global- {groupname}', f'Separado- {groupname}'],[scorejoint,score],[recalljoint,recall],[ppvjoint, ppv])),
-                    columns=['Model', 'AUC', f'Recall_{K}', f'PPV_{K}'])
-    table=pd.concat([df, table])
-fig1, (ax_sep1,ax_joint1) = plt.subplots(1,2,figsize=(10,8))
-fig2, (ax_sep2,ax_joint2) = plt.subplots(1,2,figsize=(10,8))
-for groupname in sex:
-    roc[groupname].plot(ax_sep1)
-    roc_joint[groupname].plot(ax_joint1)
-    pr[groupname].plot(ax_sep2)
-    pr_joint[groupname].plot(ax_joint2)
+    # METRICS
+    for model, preds in zip(models,[joint_preds, separate_preds, inter_preds]):
+        prec, rec, thre = precision_recall_curve(obs, preds)
+        fpr, tpr, rocthresholds = roc_curve(obs, preds)
+        FPR[groupname][model]=fpr
+        TPR[groupname][model]=tpr
+        PRECISION[groupname][model]=prec
+        RECALL[groupname][model]=rec
+        THRESHOLDS[groupname][model]=thre
+        ROCTHRESHOLDS[groupname][model]=rocthresholds
+        # CURVES - PLOTS
+        roc[groupname][model]=plot_roc(fpr, tpr, groupname)
+        pr[groupname][model]=plot_pr(prec,rec, groupname)
 
-ax_sep1.set_title('Separados')
-ax_joint1.set_title('Juntos')
-ax_sep2.set_title('Separados')
-ax_joint2.set_title('Juntos')
+        recallK,ppvK, specK=performance(preds, obs, K)
+        rocauc=roc_auc_score(obs, preds)
+        recall[model]=recallK
+        spec[model]=specK
+        score[model]=rocauc
+        ppv[model]=ppvK
+
+    df=pd.DataFrame(list(zip([f'{model}- {groupname}' for model in models],
+                             [score[model] for model in models],[recall[model] for model in models],[spec[model] for model in models],[ppv[model] for model in models])),
+                    columns=['Model', 'AUC', f'Recall_{K}',f'Specificity_{K}', f'PPV_{K}'])
+    table=pd.concat([df, table])
+
+axhist.legend(prop={'size': 10})
+axhist2.legend(prop={'size': 10})
+fig1, (ax_sep1,ax_joint1, ax_inter1) = plt.subplots(1,3,figsize=(10,8))
+fig2, (ax_sep2,ax_joint2, ax_inter2) = plt.subplots(1,3,figsize=(10,8))
+for groupname in sex:
+    for ax, model in zip((ax_sep1,ax_joint1, ax_inter1), models):
+        roc[groupname][model].plot(ax)
+        ax.set_title(model)
+    for ax, model in zip((ax_sep2,ax_joint2, ax_inter2), models):
+        pr[groupname][model].plot(ax)
+        ax.set_title(model)
 
 print(table.to_markdown(index=False,))
-features=X.drop(['PATIENT_ID','FEMALE'],axis=1).columns
-globalFeatures=X.drop(['PATIENT_ID'],axis=1).columns #preserves the order
+#%%
+interFeatures=X.columns #preserves the order
+globalFeatures=features #original columns (without interaction terms)
+separateFeatures=[f for f in features if not f=='FEMALE'] #for the separate models
+
 timesLargerForMen,ratio,m1,m2=beta_differences('logisticHombres.joblib', 'logisticMujeres.joblib',
-                            features)
-oddsContribMuj={name:np.exp(value) for name, value in zip(features, m2.coef_[0])}
-oddsContribHom={name:np.exp(value) for name, value in zip(features, m1.coef_[0])}
+                            separateFeatures)
+oddsContribMuj={name:np.exp(value) for name, value in zip(separateFeatures, m2.coef_[0])}
+oddsContribHom={name:np.exp(value) for name, value in zip(separateFeatures, m1.coef_[0])}
 oddsContribGlobal={name:np.exp(value) for name, value in zip(globalFeatures, globalmodel.coef_[0])}
+oddsContribInter={name:np.exp(value) for name, value in zip(interFeatures, interactionmodel.coef_[0])}
 print('Global contrib of FEMALE variable is: ', oddsContribGlobal['FEMALE'])
 
 K=5
@@ -146,7 +205,8 @@ print(top_K_dict(timesLargerForMen, K, reverse=False))
 oddsContrib={name:[muj, hom] for name, muj, hom in zip(features, oddsContribMuj.values(), oddsContribHom.values())}
 oddsContrib=pd.DataFrame.from_dict(oddsContrib,orient='index',columns=['Mujeres', 'Hombres'])
 oddsContrib['Global']=pd.Series(oddsContribGlobal)
-femaleContrib=pd.Series([np.nan, np.nan , oddsContribGlobal['FEMALE']],index=['Mujeres', 'Hombres', 'Global'],name='FEMALE')
+oddsContrib['Interaccion']=pd.Series(oddsContribInter)
+femaleContrib=pd.Series([np.nan, np.nan , oddsContribGlobal['FEMALE'],oddsContribInter['FEMALE']],index=['Mujeres', 'Hombres', 'Global', 'Interaccion'],name='FEMALE')
 oddsContrib.loc['FEMALE']=femaleContrib
 Xhom=X.loc[male]
 Xmuj=X.loc[female]
@@ -156,3 +216,19 @@ oddsContrib['NHom']=[Xhom[name].sum() for name in oddsContrib.index]
 
 
 oddsContrib.to_csv(config.MODELPATH+'sexSpecificOddsContributions.csv')
+#%%
+for i,model in enumerate(models):
+    print(' ')
+    print(model)
+    print('what should the probability threshold be to get the same recall for women as for men? ')
+    print('Recall for men (global model): ',table.Recall_20000.iloc[i])
+    idx=[ n for n,i in enumerate(RECALL['Mujeres'][model]) if i<=table.Recall_20000.iloc[0]][0]
+    t=THRESHOLDS['Mujeres'][model][idx]
+    idx2=[ n for n,i in enumerate(ROCTHRESHOLDS['Mujeres'][model]) if i<=t][0]
+    print('Threshold: ',t)
+    print('Recall: ', RECALL['Mujeres'][model][idx])
+    print('Number of selected women: ', sum(joint_preds>=t))
+    print('Specificity for these women is: ',1-FPR['Mujeres'][model][idx2])
+    print('ANd for men: ',table.Specificity_20000.iloc[i])
+    print('PPV for these women is: ',PRECISION['Mujeres'][model][idx])
+    print('ANd for men: ',table.PPV_20000.iloc[i])
