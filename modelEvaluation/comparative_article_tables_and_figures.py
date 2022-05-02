71e7886fb103f99e06d25aad39b2a11093d3a5bc
AlexOlza <aolza@bcamath.org>
2022-05-02 12:54:33 +0200

    Begin demography table

---
+++ modelEvaluation/comparative_article_tables_and_figures.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  2 12:24:55 2022
+
+@author: aolza
+"""
+import sys
+sys.path.append('/home/aolza/Desktop/estratificacion/')#necessary in cluster
+
+chosen_config='configurations.cluster.logistic'
+experiment='configurations.urgcms_excl_nbinj'
+import importlib
+importlib.invalidate_caches()
+from python_settings import settings as config
+logistic_settings=importlib.import_module(chosen_config,package='estratificacion')
+if not config.configured:
+    config.configure(logistic_settings) # configure() receives a python module
+assert config.configured 
+import configurations.utility as util
+util.makeAllPaths()
+
+from dataManipulation.dataPreparation import getData
+import pandas as pd
+import numpy as np
+#%%
+
+X,y=getData(2016)
+X17,y18=getData(2017)
+#%%
+""" TABLE 1:  DEMOGRAPHICS"""
+female=X['FEMALE']==1
+male=X['FEMALE']==0
+sex=[ 'Women','Men']
+table1={'Women':pd.DataFrame(), 'Men':pd.DataFrame()}
+
+for group, groupname in zip([female,male],sex):
+    print(groupname)
+    Xgroup=X.loc[group]
+    ygroup=y.loc[group]
+    # ygroup18=y.loc[group18]
+    positives=sum(np.where(ygroup.urgcms>=1,1,0))
+    table1[groupname]['N (%)']=[f'{len(Xgroup)} ({len(Xgroup)*100/len(X):2.2f} %)']
+    table1[groupname]['Hospitalized in 2017']=[f'{positives} ({positives*100/len(Xgroup):2.2f} %)']
+    # table1[groupname]['Hospitalized (2018)']=sum(np.where(ygroup18.urgcms>=1,1,0))
+    
