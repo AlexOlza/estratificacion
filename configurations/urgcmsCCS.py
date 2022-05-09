49b1f3c64f07176d0f615a10e4658489510139f8
AlexOlza <aolza@bcamath.org>
2022-05-09 15:57:47 +0200

    Add new experiment

---
+++ configurations/urgcmsCCS.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  9 15:55:10 2022
+
+@author: aolza
+"""
+
+EXPERIMENT='urgcmsCCS'
+CONFIGNAME='urgcmsCCS.py'
+COLUMNS=['urgcms']
+TRACEBACK=True
+
+"""PREDICTORS: They will be different for each script."""
+PREDICTORREGEX=r'PATIENT_ID'
+INDICEPRIVACION=False
+COLUMNS=['urgcms']#variable respuesta
+PREVIOUSHOSP=[]
+EXCLUDE=['nbinj']
+#Exclude patients from Tolosaldea and Errioxa because they receive
+#most of their care outside of Osakidetza.
+EXCLUDEOSI=['OS16','OS22'] 
+RESOURCEUSAGE=False
+
+""" CCS"""
+CCS=True
+ICDFILES={2016:'ccs/dx_in_2016.txt',
+          2017:'ccs/dx_in_2017.txt'}
+ICDTOCCSFILES={'ICD10CM':'ccs/translate_icd10cm_ccs_2018.csv',
+               'ICD9':'ccs/translate_icd9_ccs_2015.csv'}
+CCSFILES={2016:'CCS2016.csv',
+          2017: 'CCS2017.csv'}
