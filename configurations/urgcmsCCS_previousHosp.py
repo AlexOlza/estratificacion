90448f4687469868765f7621b3e257e7c16c2f63
AlexOlza <aolza@bcamath.org>
2022-05-27 10:06:19 +0200

    New experiment

---
+++ configurations/urgcmsCCS_previousHosp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 10:03:27 2022
+
+@author: aolza
+"""
+EXPERIMENT='urgcmsCCS_previousHosp'
+CONFIGNAME='urgcmsCCS_previousHosp.py'
+COLUMNS=['urgcms']
+TRACEBACK=True
+
+"""PREDICTORS: They will be different for each script."""
+PREDICTORREGEX=r'PATIENT_ID|AGE|FEMALE|CCS|ingresoPrevio'
+INDICEPRIVACION=False
+COLUMNS=['urgcms']#variable respuesta
+EXCLUDE=['nbinj']
+#Exclude patients from Tolosaldea and Errioxa because they receive
+#most of their care outside of Osakidetza.
+EXCLUDEOSI=['OS16','OS22'] 
+RESOURCEUSAGE=False
+
+""" CCS"""
+CCS=True
+ICDFILES={2016:'ccs/dx_in_2016.txt',
+          2017:'ccs/dx_in_2017.txt'}
+ICDTOCCSFILES={'ICD10CM':'ccs/translate_icd10cm_ccs_2018.csv',
+               'ICD9':'ccs/translate_icd9_ccs_2015.csv'}
+CCSFILES={2016:'CCS2016.csv',
+          2017: 'CCS2017.csv'}
