0d8d108bc3dbd3a09a5fcdce4bee27b3b425352f
AlexOlza <aolza@bcamath.org>
2022-07-06 12:17:25 +0200

    Fit gender-balanced LR

--- main/cluster/logistic_gender_balanced.py
+++ main/cluster/logistic_gender_balanced.py
@@ -23,16 +23,16 @@ util.makeAllPaths()
 
 from dataManipulation.dataPreparation import getData
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 #%%
 np.random.seed(config.SEED)
 
 X,y=getData(2016)
-#%%
+#%% 
 def gender_balanced_undersampling(X,y):
     import itertools
-    import pandas as pd
     XFemale=X.loc[X.FEMALE==1]
     yFemale=y.loc[XFemale.index]
     XMale=X.loc[X.FEMALE==0]
@@ -51,17 +51,29 @@ def gender_balanced_undersampling(X,y):
     idx=np.array(list(itertools.chain.from_iterable((yMale[config.COLUMNS] >= 1).values)))
     ing_indices = yMale.loc[idx].index
     noing_indices = yMale.loc[(~idx)].index
-    half_sample_size = len(ing_indices)  # Equivalent to len(data[data.Healthy == 0])
     random_indices = np.random.choice(noing_indices, half_sample_size, 
                                   replace=False)
-    
+    ing_indices = np.random.choice(ing_indices, half_sample_size, 
+                                  replace=False)
     XMaleBalanced=pd.concat([XMale.loc[random_indices],XMale.loc[ing_indices]])
     yMaleBalanced=pd.concat([yMale.loc[random_indices],yMale.loc[ing_indices]])
-    return(XFemaleBalanced,yFemaleBalanced,XMaleBalanced,yMaleBalanced)
+    
+    #this would be 0.5 always in the current implementation
+    prevalence_Females=sum(np.where(yFemaleBalanced[config.COLUMNS]>=1,1,0))/len(yFemaleBalanced)
+    prevalence_Males=sum(np.where(yMaleBalanced[config.COLUMNS]>=1,1,0))/len(yMaleBalanced)
+    
+    assert sum(np.where(yMaleBalanced[config.COLUMNS]>=1,1,0))[0]==len(XMaleBalanced)/2, 'Hospitalizations incorrectly balanced for males'
+    assert sum(np.where(yFemaleBalanced[config.COLUMNS]>=1,1,0))[0]==len(XFemaleBalanced)/2, 'Hospitalizations incorrectly balanced for females'
+    assert len(XMaleBalanced)==len(XFemaleBalanced), 'Gender unbalanced'
+    assert prevalence_Females==prevalence_Males, 'Unequeal prevalences'
+    
+    return(XFemaleBalanced,yFemaleBalanced,XMaleBalanced,yMaleBalanced,prevalence_Females)
 
 Xf,yf,Xm,ym=gender_balanced_undersampling(X,y)
+
+X=pd.concat([Xf,Xm])
+y=pd.concat([yf,ym])
 #%%
-assert False
 print('first IDs (X)')
 print(X.head().PATIENT_ID)
 try:
@@ -69,16 +81,11 @@ try:
 except:
     pass
 
-y=pd.concat([y17.loc[random_indices],y17.loc[ing_indices]])
-y.drop(na_indices,axis=0,inplace=True)
 print('first IDs (y)')
 print(y.head().PATIENT_ID)
 y=np.where(y[config.COLUMNS]>=1,1,0)
 y=y.ravel()
-print('Dropping NA. Amount: ',len(na_indices))
 print('Sample size ',len(X))
-np.random.seed(seed_hparam)
-X,y=gender_balanced_undersampling(X,y)
 #%%
 
 y=np.where(y[config.COLUMNS]>=1,1,0)
