d0bfe9823eba44f872407b3dc193e8b28369d53b
AlexOlza <aolza@bcamath.org>
2022-10-13 13:24:25 +0200

    Adapt to run as main with neural regression

--- modelEvaluation/predict.py
+++ modelEvaluation/predict.py
@@ -186,6 +186,12 @@ def predict(model_name,experiment_name,year,**kwargs):
     return (probs,score)
 #%%
 if __name__=='__main__':
-        
+    from keras import backend as K
+       
+    def coeff_determination(y_true, y_pred):
+        SS_res =  K.sum(K.square( y_true-y_pred )) 
+        SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) ) 
+        return ( 1 - SS_res/(SS_tot + K.epsilon()) )
     year=int(input('YEAR YOU WANT TO PREDICT:'))
-    probs,score=predict(model_name,experiment_name,year)       
+    custom_objects={'coeff_determination':coeff_determination} if 'neuralRegression' in config.CONFIGNAME else None
+    probs,score=predict(model_name,experiment_name,year,custom_objects=custom_objects)       
