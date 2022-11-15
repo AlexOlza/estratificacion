0d3cf72923f7df3ff5c30c26bce2c3100e839310
AlexOlza <aolza@bcamath.org>
2022-11-15 12:06:43 +0100

    Reorganize code

--- main/gender_analysis/propensity_score_matching_nearest_neighbours.py
+++ main/gender_analysis/propensity_score_matching_nearest_neighbours.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+
+a reference: https://sci-hub.se/10.1213/ANE.0000000000002787
 Created on Tue Nov  8 10:32:34 2022
 
 @author: aolza
@@ -22,52 +24,92 @@ import configurations.utility as util
 util.makeAllPaths()
 
 import pandas as pd
+import seaborn as sns
+import joblib as job
+from matplotlib import pyplot as plt
 from dataManipulation.dataPreparation import getData
+from modelEvaluation.compare import performance
 import numpy as np
 import os
 from time import time
 from pathlib import Path
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import roc_auc_score, average_precision_score, r2_score, mean_squared_error
+#%%
+def logit(x, eps=1e-16):
+    if x==0:
+        return -1e6
+    if x==1:
+        return 1e6
+    logit_=np.log(x)-np.log(1-x)
+    return logit_
+def perfom_matching_v2(row, indexes, df_data): #MATCHES WITH REPLACEMENT!! 
+    current_index = int(row['index']) # Obtain value from index-named column, not the actual DF index.
+    for idx in indexes[current_index,:]:
+        if (current_index != idx) and (row.FEMALE == 1) and (df_data.loc[idx].FEMALE == 0):
+            return int(idx)
+        
 
+def get_matching_pairs(treated, non_treated, caliper):
+    nbrs = NearestNeighbors(n_neighbors=1,n_jobs=-1, radius=caliper).fit(non_treated)
+    distances, indices = nbrs.kneighbors(treated)
+    indices = indices.reshape(indices.shape[0])
+    matched = non_treated.iloc[indices]
+    return matched
 #%%
 np.random.seed(config.SEED)
-
-X,y=getData(2016)
+no_duplicates=eval(input('Drop duplicates? (True/False) '))
+plot=eval(input('Make plots? (True/False) '))
+evaluate_matching=eval(input('Evaluate matching? (True/False) '))
 #%%
-filename=os.path.join(config.PREDPATH,'pairs.csv')
-if not Path(filename).is_file():
-    Xx=X#.sample(1000000)
-    z=Xx['FEMALE']
-    df_data =Xx
-    Xx=Xx.drop(['FEMALE', 'PATIENT_ID'], axis=1)
-    print('Sample size ',len(X), 'positive: ',sum(z))
-    assert not 'AGE_85GT' in X.columns
-    
-  
+X,y=getData(2016)
+original_columns=X.columns
+print('Sample size ',len(X), 'females: ',X.FEMALE.sum())
+assert not 'AGE_85GT' in X.columns
+
+#%%
+filename=os.path.join(config.PREDPATH,'two_neighbour_pairs.csv')
+ps_model_filename=os.path.join(config.MODELPATH,'logistic_propensity_score_model.joblib')
+#%%
+""" COMPUTE OR RELOAD PROPENSITY SCORE MODEL """
+if (not Path(ps_model_filename).is_file()):
     logistic=LogisticRegression(penalty='none',max_iter=1000,verbose=0)
     
-
-    def logit(x, eps=1e-16):
-        if x==0:
-            return -1e6
-        if x==1:
-            return 1e6
-        logit_=np.log(x)-np.log(1-x)
-        return logit_
-    from sklearn.neighbors import NearestNeighbors
     t0=time()
-    fit=logistic.fit(Xx, z)
+    fit=logistic.fit(X.drop(['FEMALE', 'PATIENT_ID'], axis=1), X['FEMALE'])
     print('fitting time: ',time()-t0)
+    util.savemodel(config, fit,name='logistic_propensity_score_model')
     
-    propensity_scores=fit.predict_proba(Xx)[:,1]
-    propensity_logit = np.array([logit(xi) for xi in propensity_scores])
+else:
+    fit=job.load(ps_model_filename)
+propensity_scores=fit.predict_proba(X.drop(['FEMALE', 'PATIENT_ID'], axis=1))[:,1]
+propensity_logit = np.array([logit(xi) for xi in propensity_scores])
+
+X.loc[:,'propensity_score'] = propensity_scores
+X.loc[:,'propensity_score_logit'] = propensity_logit
+X.loc[:,'outcome'] = y.iloc[X.index][config.COLUMNS].values
+initial_AUC=roc_auc_score(X.FEMALE, propensity_scores)
+print('Initial AUC for propensity scores: ', initial_AUC)
+#%%
+""" PERFORM MATCHING OR RELOAD PAIRS FROM FILE """
+if not Path(filename).is_file():
+   
+    X.loc[:,'propensity_score'] = propensity_scores
+    X.loc[:,'propensity_score_logit'] = propensity_logit
+    X.loc[:,'outcome'] = y.iloc[X.index][config.COLUMNS].values
     
-    
-    df_data.loc[:,'propensity_score'] = propensity_scores
-    df_data.loc[:,'propensity_score_logit'] = propensity_logit
-    df_data.loc[:,'outcome'] = y.iloc[Xx.index][config.COLUMNS].values
-    
-    
+    if plot:
+        
+        sns.set(rc={'figure.figsize':(16,10)}, font_scale=1.3)
+        # Density distribution of propensity score (logic) broken down by treatment status
+        fig, ax = plt.subplots(1,1)
+        fig.suptitle('Density distribution plots for propensity score')
+        sns.kdeplot(x = propensity_scores, hue = X.FEMALE , ax = ax)
+        ax.set_title('Propensity Score')
+        
+        plt.savefig(os.path.join(config.FIGUREPATH,'propensity_densities_before.png'))
+        plt.show()
     
     """ MATCHING """
     
@@ -77,32 +119,29 @@ if not Path(filename).is_file():
     print('\nCaliper (radius) is: {:.4f}\n'.format(caliper))#Caliper (radius) is: 0.0579
     
     
-    knn = NearestNeighbors(n_neighbors=10 , p = 2, radius=caliper,n_jobs=-1)
+    knn = NearestNeighbors(n_neighbors=2 , p = 2, radius=caliper,n_jobs=-1)
     knn.fit(propensity_logit.reshape(-1, 1))
     
     
-    df_data.index=range(len(df_data))
+    X.index=range(len(X))
     distances , indexes = knn.kneighbors(
-        df_data.propensity_score_logit.to_numpy().reshape(-1, 1), \
-        n_neighbors=10)
+        X.propensity_score_logit.to_numpy().reshape(-1, 1), \
+        n_neighbors=2)
+        
+    pairs=get_matching_pairs(X.loc[X.FEMALE==1].propensity_score_logit.to_numpy().reshape(-1, 1),
+                             X.loc[X.FEMALE==0].propensity_score_logit.to_numpy().reshape(-1, 1),
+                             caliper)
     
     print('For item 0, the 4 closest distances are (first item is self):')
     for ds in distances[0,0:4]:
         print('Element distance: {:4f}'.format(ds))
     print('...')
-    
-    def perfom_matching_v2(row, indexes, df_data): #MATCHES WITH REPLACEMENT!! 
-        current_index = int(row['index']) # Obtain value from index-named column, not the actual DF index.
-        prop_score_logit = row['propensity_score_logit']
-        for idx in indexes[current_index,:]:
-            if (current_index != idx) and (row.FEMALE == 1) and (df_data.loc[idx].FEMALE == 0):
-                return int(idx)
    
    
-    df_data['matched_element'] = df_data.reset_index().apply(perfom_matching_v2, axis = 1, args = (indexes, df_data))
+    X['matched_element'] = X.reset_index().apply(perfom_matching_v2, axis = 1, args = (indexes, X))
     
-    females=df_data.loc[~df_data.matched_element.isna()]#.drop_duplicates('matched_element')
-    males=df_data.loc[df_data.index.isin(df_data.matched_element)]
+    females=X.loc[~X.matched_element.isna()]
+    males=X.loc[X.index.isin(X.matched_element)]
 
     pairs=pd.concat([males, females])
 
@@ -111,62 +150,68 @@ if not Path(filename).is_file():
 else:
     pairs=pd.read_csv(filename)
 #%%
-plot=True
-if plot:
-    import seaborn as sns
-    from matplotlib import pyplot as plt
-    sns.set(rc={'figure.figsize':(16,10)}, font_scale=1.3)
-    # Density distribution of propensity score (logic) broken down by treatment status
-    fig, ax = plt.subplots(1,2)
-    fig.suptitle('Density distribution plots for propensity score and logit(propensity score).')
-    sns.kdeplot(x = propensity_scores, hue = z , ax = ax[0])
-    ax[0].set_title('Propensity Score')
-    sns.kdeplot(x = propensity_logit, hue = z , ax = ax[1])
-    ax[1].axvline(-0.4, ls='--')
-    ax[1].set_title('Logit of Propensity Score')
-    plt.show()
-    plt.savefig(os.path.join(config.FIGUREPATH,'propensity_densities_before.png'))
-    
-    
-    sns.set(rc={'figure.figsize':(16,10)}, font_scale=1.3)
-    # Density distribution of propensity score (logic) broken down by treatment status
-    fig, ax = plt.subplots(1,1)
-    fig.suptitle('Density distribution plots for propensity score and logit(propensity score).')
-    sns.kdeplot(x = pairs.propensity_score, hue = pairs.FEMALE , ax = ax)
-    ax.set_title('Propensity Score')
-    plt.show()
-    # plt.savefig(os.path.join(config.FIGUREPATH,'after.png'))
 
-    df_data['binary_outcome']=(df_data.outcome>0)
-    pairs['binary_outcome']=(pairs.outcome>0)
-    fig, ax = plt.subplots(2,1)
-    fig.suptitle('Comparison of {} split by outcome and treatment status'.format('propensity_score_logit'))
-    sns.stripplot(data = df_data, y = 'binary_outcome', alpha=0.5, x = 'propensity_score_logit', hue = 'FEMALE', orient = 'h', ax = ax[0]).set(title = 'Before matching', xlim=(-6, 4))
-    sns.stripplot(data = pairs, y = 'binary_outcome', alpha=0.5, x = 'propensity_score_logit', hue = 'FEMALE', ax = ax[1] , orient = 'h').set(title = 'After matching', xlim=(-6, 4))
-    plt.subplots_adjust(hspace = 0.3)
-    plt.show()
-    
+if no_duplicates:
+    females_=pairs.loc[pairs.FEMALE==1]#.drop_duplicates('matched_element')
+    males_=pairs.loc[pairs.FEMALE==0]
+    pairs=pd.concat([males_, females_]).sample(frac=1)# to shuffle the data
+    pairs.index=range(len(pairs))
 
-    sns.set(rc={'figure.figsize':(10,30)}, font_scale=1.0)
-    def cohenD (tmp, metricName):
-        treated_metric = tmp[tmp.FEMALE == 1][metricName]
-        untreated_metric = tmp[tmp.FEMALE == 0][metricName]
+pairs['outcome']=pd.merge(pairs[['PATIENT_ID']],y,on='PATIENT_ID')[config.COLUMNS]
+#%%
+threshold= 0 if config.ALGORITHM=='logistic' else y[config.COLUMNS].mean()
+X['binary_outcome']=(y[config.COLUMNS]>threshold)
+pairs['binary_outcome']=(pairs.outcome>threshold.values[0])
+if evaluate_matching:
+    if plot:
+        sns.set(rc={'figure.figsize':(16,10)}, font_scale=1.3)
+        # Density distribution of propensity score (logic) broken down by treatment status
+        fig, ax = plt.subplots(1,1)
+        fig.suptitle('Density distribution plots for propensity score and logit(propensity score).')
+        sns.kdeplot(x = pairs.propensity_score, hue = pairs.FEMALE , ax = ax)
+        ax.set_title('Propensity Score')  
+        plt.savefig(os.path.join(config.FIGUREPATH,'two_propensity_densities_after.png'))
+        plt.show()
         
-        d = ( treated_metric.mean() - untreated_metric.mean() ) / np.sqrt(((treated_metric.count()-1)*treated_metric.std()**2 + (untreated_metric.count()-1)*untreated_metric.std()**2) / (treated_metric.count() + untreated_metric.count()-2))
-        return d
-    data = []
-    cols = np.random.choice(X.columns, size=100, replace=False)
-    # cols = ['Age','SibSp','Parch','Fare','sex_female','sex_male','embarked_C','embarked_Q','embarked_S']
-    for cl in cols:
-        data.append([cl,'before', cohenD(df_data,cl)])
-        data.append([cl,'after', cohenD(pairs,cl)])
+        
+        fig, ax = plt.subplots(2,1)
+        fig.suptitle('Comparison of {} split by outcome and treatment status'.format('propensity_score_logit'))
+        sns.stripplot(data = X, y = 'binary_outcome', alpha=0.5, x = 'propensity_score_logit', hue = 'FEMALE', orient = 'h', ax = ax[0]).set(title = 'Before matching', xlim=(-6, 4))
+        sns.stripplot(data = pairs, y = 'binary_outcome', alpha=0.5, x = 'propensity_score_logit', hue = 'FEMALE', ax = ax[1] , orient = 'h').set(title = 'After matching', xlim=(-6, 4))
+        plt.subplots_adjust(hspace = 0.3)
+        plt.savefig(os.path.join(config.FIGUREPATH,'two_stripplot.png'))
+        plt.show()
+        
+        sns.set(rc={'figure.figsize':(10,60)}, font_scale=1.0)
+        def cohenD (tmp, metricName):
+            treated_metric = tmp[tmp.FEMALE == 1][metricName]
+            untreated_metric = tmp[tmp.FEMALE == 0][metricName]
+            
+            d = ( treated_metric.mean() - untreated_metric.mean() ) / np.sqrt(((treated_metric.count()-1)*treated_metric.std()**2 + (untreated_metric.count()-1)*untreated_metric.std()**2) / (treated_metric.count() + untreated_metric.count()-2))
+            return d
+        data = []
+        cols = original_columns
+        for cl in cols:
+            if cl in np.array(data).ravel()[::3]:
+                continue
+            data.append([cl,'before', cohenD(X,cl)])
+            data.append([cl,'after', cohenD(pairs,cl)])
+        
+        res = pd.DataFrame(data, columns=['variable','matching','effect_size'])
+        sns.set(rc={'figure.figsize':(10,60)}, font_scale=1.0)
+        sn_plot = sns.barplot(data = res, y = 'variable', x = 'effect_size', hue = 'matching', orient='h')
+        sn_plot.set(title='Standardised Mean differences accross covariates before and after matching')
+        sn_plot.figure.savefig(os.path.join(config.FIGUREPATH,"two_all_standardised_mean_differences.png"))
     
-    res = pd.DataFrame(data, columns=['variable','matching','effect_size'])
-    
-    sn_plot = sns.barplot(data = res, y = 'variable', x = 'effect_size', hue = 'matching', orient='h')
-    sn_plot.set(title='Standardised Mean differences accross covariates before and after matching')
-    # sn_plot.figure.savefig("standardised_mean_differences.png")
-
+    #%%
+    # Evaluate the decrease in AUC
+    logistic=LogisticRegression(penalty='none',max_iter=500,verbose=2,n_jobs=-1)
+    newfit=logistic.fit(pairs[original_columns].drop(['FEMALE','PATIENT_ID'], axis=1), pairs.FEMALE)
+    new_propensity_scores=newfit.predict_proba(pairs[original_columns].drop(['FEMALE','PATIENT_ID'], axis=1))[:,1]
+    new_AUC=roc_auc_score(pairs.FEMALE, new_propensity_scores)
+    #%%
+    print('Initial AUC for propensity scores: ', initial_AUC)
+    print('After matching, AUC for new propensity scores: ', new_AUC)
 #%%
 overview = pairs[['outcome','FEMALE']].groupby(by = ['FEMALE']).aggregate([np.mean, np.var, np.std, 'count'])
 print(overview.to_markdown())
@@ -174,34 +219,79 @@ print(overview.to_markdown())
 treated_outcome = overview['outcome']['mean'][1]
 treated_counterfactual_outcome = overview['outcome']['mean'][0]
 att = treated_outcome - treated_counterfactual_outcome
-print('The Average Treatment Effect (ATT): {:.4f}'.format(att))
+print('The Average Treatment Effect is (ATE): {:.4f}'.format(att))
 #Vemos que, en una muestra de mujeres y hombres con las mismas 
 #características clínicas, las mujeres ingresan menos (ATT<0)
 #%%
-ypairs=pairs.outcome
-x=pairs[X.columns]
-try:
-    x.drop(['PATIENT_ID'],axis=1,inplace=True)
-except:
-    pass
+overview2 = pairs[['binary_outcome','FEMALE']].groupby(by = ['FEMALE']).aggregate([np.mean, np.var, np.std, 'count'])
+print(overview2.to_markdown())
+treated_outcome = overview2['binary_outcome']['mean'][1]
+treated_counterfactual_outcome = overview2['binary_outcome']['mean'][0]
+att2 = treated_outcome - treated_counterfactual_outcome
+print('For presence/absence of outcome, the Average Treatment Effect in females is (ATT): {:.4f}'.format(att2))
 
-ypairs=np.where(ypairs>=1,1,0)
+#%%
+ypairs=pairs.outcome
+x=pairs[original_columns].drop(['PATIENT_ID'],axis=1)
+
+ypairs=np.where(ypairs>=1,1,0) if config.ALGORITHM=='logistic' else ypairs
 ypairs=ypairs.ravel()
 #%%
-logistic=LogisticRegression(penalty='none',max_iter=1000,verbose=0)
-t0=time()
-fit=logistic.fit(x, ypairs)
-print('fitting time: ',time()-t0)
-#%%
-util.savemodel(config, fit,name='logistic_psm')
-#%%
-yMale=np.where(pairs.loc[pairs.FEMALE==0].outcome>=1,1,0)
-yFemale=np.where(pairs.loc[pairs.FEMALE==1].outcome>=1,1,0)
-from sklearn.metrics import roc_auc_score, average_precision_score
-predsMale=fit.predict_proba(x.loc[x.FEMALE==0])
-print('AUC Male= ',roc_auc_score(yMale,predsMale[:,1]))
-print('AP Male= ',average_precision_score(yMale,predsMale[:,1]))
+if config.ALGORITHM=='logistic':
+    estimator=LogisticRegression(penalty='none',max_iter=1000,verbose=0,n_jobs=-1)
+    predict=estimator.predict_proba
+    score=roc_auc_score
+    score2=average_precision_score
+    kwargs_score2={}
+    score_name,score2_name='AUC','AP'
+elif config.ALGORITHM=='linear':
+    estimator=LinearRegression(n_jobs=-1)
+    predict=estimator.predict
+    score=r2_score
+    score2=mean_squared_error
+    kwargs_score2={'squared':False}
+    score_name,score2_name='R2','RMSE'  
+if not Path(os.path.join(config.MODELPATH,f'{config.ALGORITHM}_psm.joblib')).is_file():    
+    print('fitting ...')
+    t0=time()
+    fit=estimator.fit(x, ypairs)
+    print('fitting time: ',time()-t0)
+    util.savemodel(config, fit,name=f'{config.ALGORITHM}_psm')
+else:
+    fit=job.load(os.path.join(config.MODELPATH,f'{config.ALGORITHM}_psm.joblib'))
 
-predsFemale=fit.predict_proba(x.loc[x.FEMALE==1])
-print('AUC Female= ',roc_auc_score(yFemale,predsFemale[:,1]))
-print('AP Female= ',average_precision_score(yFemale,predsFemale[:,1]))
+#%%
+from sklearn.metrics import precision_recall_curve, PrecisionRecallDisplay
+def plot_pr(precision, recall, groupname,  y , pred):
+    avg_prec = average_precision_score(y, pred)
+    display = PrecisionRecallDisplay(precision=precision, recall=recall,
+                                     estimator_name=groupname,
+                                     average_precision=avg_prec)
+    return display
+if config.ALGORITHM=='logistic':
+    yMale=np.where(pairs.loc[pairs.FEMALE==0].outcome>=1,1,0)
+    yFemale=np.where(pairs.loc[pairs.FEMALE==1].outcome>=1,1,0)
+else:
+    yMale=pairs.loc[pairs.FEMALE==0].outcome
+    yFemale=pairs.loc[pairs.FEMALE==1].outcome
+predsMale=fit.predict(x.loc[x.FEMALE==0])
+predsFemale=fit.predict(x.loc[x.FEMALE==1])
+
+fig, ax = plt.subplots(1,1)
+plot={}
+for sex, yy, preds in zip(['Male', 'Female'], [yMale, yFemale], [predsMale, predsFemale]):
+    if config.ALGORITHM=='logistic':
+        preds=preds[:,1]
+    print(f'{score_name} {sex}= ',score(yy,preds))
+    print(f'{score2_name} {sex}= ',score2(yy,preds, **kwargs_score2))
+    rec, ppv, _, _=performance(yy, preds, K=20000)
+    print(f'Recall_20000 {sex}= ', rec)
+    print(f'PPV_20000 {sex}= ', ppv)
+    print(' ')
+    if config.ALGORITHM=='logistic':
+        prec, rec, thre = precision_recall_curve(yy, preds)
+        plot[sex]=plot_pr(prec, rec, sex, yy, preds).plot(ax)
+#%%
+if config.ALGORITHM=='logistic':
+    for sex in ['Male', 'Female']:
+        plot[sex].plot(ax)
