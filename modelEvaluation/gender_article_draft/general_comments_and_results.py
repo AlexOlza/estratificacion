27721aa4c34f21af07b6a4dd1e9aa63088499725
AlexOlza <aolza@bcamath.org>
2023-04-18 16:06:44 +0200

    Begin creating big table of results

--- modelEvaluation/gender_article_draft/general_comments_and_results.py
+++ modelEvaluation/gender_article_draft/general_comments_and_results.py
@@ -24,12 +24,19 @@ util.makeAllPaths()
 import joblib
 from dataManipulation.dataPreparation import getData
 import pandas as pd
+import re
 
-logistic_modelpath=config.ROOTPATH+'/models/urgcmsCCS_parsimonious/'
-linear_modelpath=config.ROOTPATH+'/models/costCCS_parsimonious/'
+logistic_modelpath=config.ROOTPATH+'models/urgcmsCCS_parsimonious/'
+linear_modelpath=config.ROOTPATH+'models/costCCS_parsimonious/'
 
-logistic_global_model=joblib.load(logistic_modelpath+'logistic20230324_111354.joblib')
-linear_global_model=joblib.load(linear_modelpath+'linear20230324_130625.joblib')
+logistic_modelname='logistic20230324_111354'
+linear_modelname='linear20230324_130625'
+
+logistic_predpath=re.sub('models','predictions',logistic_modelpath)
+linear_predpath=re.sub('models','predictions',linear_modelpath)
+
+logistic_global_model=joblib.load(logistic_modelpath+f'{logistic_modelname}.joblib')
+linear_global_model=joblib.load(linear_modelpath+f'{linear_modelname}.joblib')
 logistic_sameprev_model=joblib.load(logistic_modelpath+'logistic_gender_balanced.joblib')
 
 
@@ -44,3 +51,40 @@ print('logistic same prevalence female coef:',log_sameprev_coefs.loc['FEMALE'].v
 print('linear global female coef:', lin_global_coefs.loc['FEMALE'].values  )
 
 
+"""
+HOSPITALIZATION BIG TABLE
+"""
+def concat_preds(file1,file2):
+    muj=pd.read_csv(file1)
+    muj['FEMALE']=1
+    hom=pd.read_csv(file2)
+    hom['FEMALE']=0
+    return pd.concat([muj,hom])
+import numpy as np
+def table(path, modelname):
+    # Global model
+    global_preds=concat_preds(path+f'{modelname}_Mujeres_calibrated_2018.csv',
+                              path+f'{modelname}_Hombres_calibrated_2018.csv')   
+    # Separate models
+    modeltype=re.sub('[0-9]|_','',modelname) #this string will contain either "logistic" or "linear"
+    separate_preds=concat_preds(path+f'{modeltype}Mujeres_calibrated_2018.csv',
+                                path+f'{modeltype}Hombres_calibrated_2018.csv')    
+    if modeltype=='logistic':
+        # Same prevalence model
+        sameprevalence_preds=concat_preds(path+'logistic_gender_balanced_Mujeres_calibrated_2018.csv',
+                                          path+'logistic_gender_balanced_Hombres_calibrated_2018.csv')
+        
+        # Overall metrics: auc and brier
+        from sklearn.metrics import roc_auc_score,brier_score_loss 
+        def AUC_muj(x): return roc_auc_score(np.where(x.loc[x.FEMALE==1].OBS,1,0),x.loc[x.FEMALE==1].PRED)
+        def AUC_hom(x): return roc_auc_score(np.where(x.loc[x.FEMALE==0].OBS,1,0),x.loc[x.FEMALE==0].PRED)
+        def brier_muj(x): return brier_score_loss(np.where(x.loc[x.FEMALE==1].OBS,1,0),x.loc[x.FEMALE==1].PRED)
+        def brier_hom(x): return brier_score_loss(np.where(x.loc[x.FEMALE==0].OBS,1,0),x.loc[x.FEMALE==0].PRED)
+        
+        overall_metrics=[[AUC_muj(x),AUC_hom(x),brier_muj(x),brier_hom(x)] for x in [global_preds,separate_preds,sameprevalence_preds]]
+    else:
+        
+        #Overall metrics: R2 and RMSE
+        
+        pd.DataFrame(overall_metrics,columns=['AUC_women','AUC_men','brier_women','brier_men'],index=['global','separate','same_prevalence'])
+        
