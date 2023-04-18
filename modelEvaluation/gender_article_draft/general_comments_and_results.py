692d850f4189439adde3db563c4f845d8a6a755d
AlexOlza <aolza@bcamath.org>
2023-04-18 17:05:20 +0200

    Add threshold metrics for the 20k list

--- modelEvaluation/gender_article_draft/general_comments_and_results.py
+++ modelEvaluation/gender_article_draft/general_comments_and_results.py
@@ -60,6 +60,16 @@ def concat_preds(file1,file2):
     hom=pd.read_csv(file2)
     hom['FEMALE']=0
     return pd.concat([muj,hom])
+def patient_selection(x,modeltype):
+    x['top20k']=np.where(x.PATIENT_ID.isin(x.nlargest(20000,'PRED').PATIENT_ID),1,0)
+    top10k_women=x.loc[x.FEMALE==1].nlargest(10000,'PRED')
+    top10k_men=x.loc[x.FEMALE==0].nlargest(10000,'PRED')
+    x['top10k_gender']=np.where(x.PATIENT_ID.isin(pd.concat([top10k_women,top10k_men]).PATIENT_ID),1,0)
+    if modeltype=='linear':
+        x['should_be_selected']=np.where(x.PATIENT_ID.isin(x.nlargest(20000,'OBS').PATIENT_ID),1,0)
+    else:
+        x['should_be_selected']=np.where(x.OBS>=1,1,0)
+    return x
 import numpy as np
 def table(path, modelname):
     # Global model
@@ -85,6 +95,37 @@ def table(path, modelname):
     else:
         
         #Overall metrics: R2 and RMSE
+        from sklearn.metrics import r2_score,mean_squared_error 
+        def R2_muj(x): return r2_score(x.loc[x.FEMALE==1].OBS,x.loc[x.FEMALE==1].PRED)
+        def R2_hom(x): return r2_score(x.loc[x.FEMALE==0].OBS,x.loc[x.FEMALE==0].PRED)
+        def RMSE_muj(x): return mean_squared_error(x.loc[x.FEMALE==1].OBS,x.loc[x.FEMALE==1].PRED,squared=False)
+        def RMSE_hom(x): return mean_squared_error(x.loc[x.FEMALE==0].OBS,x.loc[x.FEMALE==0].PRED,squared=False)
         
-        pd.DataFrame(overall_metrics,columns=['AUC_women','AUC_men','brier_women','brier_men'],index=['global','separate','same_prevalence'])
+        
+    # PATIENT SELECTION:
+    # We use two criteria: 
+        # 1) Select the top 20k patients as positive, regardless of gender -> top20k
+        # 2) Select the top 10k women and top 10k men -> top10k_gender
+    
+    global_preds=patient_selection(global_preds,modeltype)
+    separate_preds=patient_selection(separate_preds,modeltype)
+    sameprevalence_preds=patient_selection(sameprevalence_preds,modeltype)
+    
+    
+    # In either case, we use the same threshold-specific metrics
+    from sklearn.metrics import confusion_matrix
+    def cm(x,col): return {key: val for key, val in zip(['tn', 'fp', 'fn', 'tp'],confusion_matrix(x[f'should_be_selected'],x[col]).ravel())} 
+    def threshold_muj_hom(x,col):
+        c=cm(x.loc[x.FEMALE==1],col)
+        vpp_women, vpn_women, sens_women, esp_women=c['tp']/(c['tp']+c['fp']),    c['tn']/(c['tn']+c['fn']),    c['tp']/(c['tp']+c['fn']),    c['tn']/(c['tn']+c['fp'])
+        c=cm(x.loc[x.FEMALE==0],col)
+        vpp_men, vpn_men, sens_men, esp_men=c['tp']/(c['tp']+c['fp']),    c['tn']/(c['tn']+c['fn']),    c['tp']/(c['tp']+c['fn']),    c['tn']/(c['tn']+c['fp'])
+        return(vpp_women, vpp_men, vpn_women, vpn_men, sens_women, sens_men, esp_women, esp_men)
+
+    threshold_metrics_20k=[threshold_muj_hom(x, 'top20k') for  x in [global_preds,separate_preds,sameprevalence_preds]]
+    
+    # threshold_metrics=np.array(threshold_metrics).ravel()
+    
+    
+    pd.DataFrame(threshold_metrics_20k,columns=['PPV_women','PPV_men','NPV_women','NPV_men','SENS_women','SENS_men','SPEC_women','SPEC_men'],index=['global','separate','same_prevalence'])
         
