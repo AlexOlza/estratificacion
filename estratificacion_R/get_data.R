09320802901354145262b0bace9699ec8b7f7817
AlexOlza <aolza@bcamath.org>
2023-05-17 09:31:29 +0200

    Add data description

--- estratificacion_R/get_data.R
+++ estratificacion_R/get_data.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+# (La linea de arriba es necesaria)
 source('/home/aolza/Desktop/estratificacion/estratificacion_R/configuracion.R')
 source('/home/aolza/Desktop/estratificacion/estratificacion_R/crear_tablas_CCS.R')
 source('/home/aolza/Desktop/estratificacion/estratificacion_R/crear_tablas_ATC.R')
@@ -8,16 +10,33 @@ get_data <- function(year, binarize=TRUE){
   X   <- merge(ccs,atc,on='PATIENT_ID')
   return(X)
 }
-
+if (sys.nframe() == 0){
 X<- get_data(2016)
 cost <- fread(as.character(config$ficheros_ACG['2016']),select=c('PATIENT_ID','COSTE_TOTAL_ANO2'))
+descrip<-fread(config$fichero_descripciones_ccs)
 
-Xy <- merge(X,cost, on='PATIENT_ID')
+##################################################################################################
+###                 DESCRIPCIÓN DE LOS DATOS
+##################################################################################################
+
+dt <- data.table()
+dt$CATEGORIES<-names(X[,-'PATIENT_ID'])
+dt$N<-colSums(X[,-'PATIENT_ID'])
+dt$percentage <- 100*dt$N/nrow(X)
+dt <- merge(dt, descrip, on='CATEGORIES', all.x = TRUE)
+
+# Pacientes con diabetes:
+dt[dt$LABELS %like% '[Dd]iabetes',]
+# Pacientes con hipertensión
+dt[dt$LABELS %like% '[Hh]ypertension',]
+##################################################################################################
+###                 MODELO DE REGRESIÓN LINEAL
+##################################################################################################
 # La memoria no da para el modelo completo, lo hago con una muestra de los datos:
+Xy <- merge(X,cost, on='PATIENT_ID')
 sampleXy <-sample_n(Xy,100000)
 model <- lm(COSTE_TOTAL_ANO2~., sampleXy[,-'PATIENT_ID'])
 # Los coeficientes NO SON VÁLIDOS, pero se podrían describir así
-descrip<-fread(config$fichero_descripciones_ccs)
 coefs<-data.table(model$coefficients)
 setnames(coefs,'beta')
 coefs$CATEGORIES <- names(model$coefficients)
@@ -25,3 +44,4 @@ coefs <- as.data.table(merge(coefs, descrip, on='CATEGORIES', all.x = TRUE))
 print(coefs[order(-beta)])
 print(coefs[coefs$CATEGORIES=='FEMALE'])
 print(coefs[startsWith(coefs$CATEGORIES,'AGE')])
+}
